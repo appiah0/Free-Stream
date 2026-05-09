ad9f5bf4d3aa84843ca391a0b9be4c5ee592a64f
appiah0 <111425714+appiah0@users.noreply.github.com>
2026-05-09 21:58:03 +0000

    Add main.jsx to render FreeStream component

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FreeStream from './FreeStream'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<FreeStream />)
